31c841c19f14c6e53fb3ac8bd7685768b006bcd4
Hugo Charles <hugo.y.charles@gmail.com>
2019-09-22 15:20:30 +0200

    User 1

--- src/main.cpp
+++ src/main.cpp
@@ -8,7 +8,7 @@ int main() {
   Road road(150);
 
   // First student must give a name to bike
-  Bike bike("");
+  Bike bike("User1");
 
   // Second student must give a name to bike2
   Bike bike2("");
